19459954e1da748d1a9c86a18651ed50ed7c2eed
smais007 <smaisshawon@gmail.com>
2024-04-26 23:21:42 +0600

    insert data in database

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dmdmzzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dmdmzzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,20 +20,30 @@ const client = new MongoClient(uri, {
   },
 });
 
-
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const placeCollection = client.db("AdventureAxisDB").collection("place");
+
+    app.get('/places', async(req, res) =>{
+        const cursor = placeCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.post("/places", async (req, res) => {
+      const addPlace = req.body;
+      console.log(addPlace);
+      const result = await placeCollection.insertOne(addPlace);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
